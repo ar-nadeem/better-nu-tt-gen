e70e3cb47b6edd771a6d48046587a6dc252cfa8c
AbdulRahman Nadeem <ar-nadeem@outlook.com>
2023-08-14 00:31:46 +0500

    Setup
    Vite + Tailwind + Next-UI + React

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import { useTheme } from "next-themes";
+
+function App() {
+  const { theme, setTheme } = useTheme();
+
+  return (
+    <>
+      <div></div>
+    </>
+  );
+}
+
+export default App;
